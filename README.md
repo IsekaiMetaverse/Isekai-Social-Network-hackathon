7d6e0591dfa13a4395fd6a1fb6798044d42e5e50
IsekaiMetaverse <isekaimetaverse@outlook.com>
2022-01-03 23:41:34 +0800

    master

---
+++ README.md
@@ -0,0 +1,1 @@
+# Isekai Metaverse Social Network
---
+++ src/components/gstyles.js
@@ -0,0 +1,11 @@
+const glStyles = {
+    card: {
+      boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+      backgroundColor: "#6667AB",
+      border: "1px solid #e7eaf3",
+      borderRadius: "0.5rem",
+      fontSize: "16px",
+      color: "#ffffff",
+    },
+  };
+  export default glStyles;
